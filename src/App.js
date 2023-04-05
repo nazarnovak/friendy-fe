d990f732a571cdf566724407bf86c2d0c5d10826
Nazar Novak <nazar.novac_c@sandvik.com>
2023-04-05 19:51:23 +0200

    Some slice 0 preparations

--- src/App.js
+++ src/App.js
@@ -2,40 +2,11 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 // import { Mixpanel } from './Mixpanel.js';
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
-import {
-  useStripe,
-  useElements,
-  PaymentElement,
-} from "@stripe/react-stripe-js";
-
 import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 
-import videoFile from "./friends.mp4";
-
-// // Test
-// const publicKey =
-//   "pk_test_51MoTPZHbbDaXSUVRdOICX0kGZr14MGxj4DYIDvSQnRmwUv7N2G42pHHVQWU2PWvrthkwYS8RJnZZH0Vhw2K3rcST00c9sxuq8F";
-
-// Live
-const publicKey =
-  "pk_live_51MoTPZHbbDaXSUVRMJhsEjHVhGnsUGUcAXtZgF8GYpWZoVV37JXyKohVILnV2tnLnYvjlWjGKX4N5GWOzMH7TnH300aEBuf1di";
-
-// For now I just generated it with curl, needs to be on the BE tho per every transaction:
-// (-u accepts the secret key, saved in stripe_secret_key locally, but not pushed,
-// or sk_ value from the API keys on the Stripe website)
-// curl https://api.stripe.com/v1/payment_intents -d amount=100 -d currency=eur -d "payment_method_types[]"=card -u <sk_> -d "capture_method"=manual
-const clientSecret =
-  "pi_3Mt5YJHbbDaXSUVR0k9QgTHA_secret_UOctRccIAtNOPWx2LQ8pOqFki";
-
-const stripePromise = loadStripe(publicKey);
-
-const options = {
-  // passing the client secret obtained from the server
-  clientSecret: clientSecret,
-};
+// Stripe
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise, stripeOptions, Stripe } from "./Stripe";
 
 const App = () => {
   let statusText = "Make a friend";
@@ -48,46 +19,41 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <Elements stripe={stripePromise} options={options}>
-        <BrowserRouter>
+      <BrowserRouter>
+        <Elements stripe={stripePromise} options={stripeOptions}>
           <Routes>
             <Route exact path="/you" element={<You />} />
             <Route exact path="/friends" element={<Friends />} />
             <Route exact path="/search" element={<Search />} />
+
+            <Route exact path="/signup" element={<SignUp />} />
             <Route exact path="/" element={<Landing />} />
           </Routes>
-        </BrowserRouter>
-      </Elements>
+        </Elements>
+      </BrowserRouter>
     </div>
   );
 };
 
 const Header: React.Component = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModalWindow = () => {
-    setModalOpen(true);
-  };
-
-  const closeModalWindow = () => {
-    setModalOpen(false);
-  };
-
   return (
     <React.Fragment>
       <header id="header">
         <div id="logo">friendy</div>
-        <ContactUsButton handleClick={openModalWindow} />
-        <ContactUsModal isOpen={modalOpen} closeModal={closeModalWindow} />
+        <SignInButton />
       </header>
     </React.Fragment>
   );
 };
 
-const ContactUsButton = ({ handleClick }) => {
+const SignInButton = () => {
+  const handleClick = () => {
+    window.location.href = "/sign-in";
+  };
+
   return (
-    <button id="contact-us" onClick={handleClick}>
-      Contact us
+    <button id="sign-in" onClick={handleClick}>
+      Sign in
     </button>
   );
 };
@@ -102,6 +68,23 @@ const ContactUsModal = ({ isOpen, closeModal }) => {
 
 const Status = ({ text }) => {
   return <div id="status">{text}</div>;
+};
+
+const CTA = ({ setEmail, handleCTAClick }) => {
+  return (
+    <div className="centered-content">
+      <input
+        id="email"
+        name="email"
+        style={{ color: "black" }}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <button id="cta" onClick={handleCTAClick}>
+        Make a best friend
+      </button>
+    </div>
+  );
 };
 
 const Landing = () => {
@@ -198,66 +181,19 @@ const Landing = () => {
     connectToWs();
   }, []);
 
-  // Stripe
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
-
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    const result = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: "https://google.com/?q=hehe",
-      },
-    });
-
-    if (result.error) {
-      // Show error to your customer (for example, payment details incomplete)
-      console.log(result.error.message);
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
-  };
-
-  // TODO: Is this (video) cool or what!?
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <PaymentElement />
-        <button disabled={!stripe}> Pay </button>
-      </form>
-      <div id="video-tint">
-        <video id="video" autoPlay loop muted>
-          <source src={videoFile} type="video/mp4" />
-        </video>
-      </div>
+    <div id="landing">
       <div id="content">
-        <h1>
-          Find likedminded people. Talk about what's important to you. Make
-          lifelong friends
-        </h1>
+        <h1>Chat with your new best friend that</h1>
+        <h1>supports you</h1>
         <div>
-          Are you tired of how hard it is to find quality friends online? Do you
-          feel like you don’t know where to search for them? Maybe you find
-          people that seem cool, but eventually you lose interest or get
-          ghosted?
+          Ready to find someone who understand you and makes you very happy?
+          Enter your email to start your journey now
         </div>
 
+        <CTA setEmail={setEmail} handleCTAClick={handleCTAClick} />
         <div id="how-does-it-work">
           <h1>How does it work</h1>
-          <div className="showcase-screenshot"></div>
           <div className="body-bold">1. Tell us about yourself</div>
           <div>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -266,8 +202,8 @@ const Landing = () => {
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting
           </div>
+          <div className="showcase-screenshot"></div>
 
-          <div className="showcase-screenshot"></div>
           <div className="body-bold">1. Tell us about yourself</div>
           <div>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -276,8 +212,8 @@ const Landing = () => {
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting
           </div>
+          <div className="showcase-screenshot"></div>
 
-          <div className="showcase-screenshot"></div>
           <div className="body-bold">1. Tell us about yourself</div>
           <div>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -286,20 +222,9 @@ const Landing = () => {
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting
           </div>
+          <div className="showcase-screenshot"></div>
         </div>
-
-        <div className="centered-content">
-          <input
-            id="email"
-            name="email"
-            style={{ color: "black" }}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <button id="cta" onClick={handleCTAClick}>
-            Make a best friend
-          </button>
-        </div>
+        <CTA setEmail={setEmail} handleCTAClick={handleCTAClick} />
       </div>
     </div>
   );
@@ -354,6 +279,12 @@ const You = () => {
       </a>
     </div>
   );
+};
+
+// TODO: Pass email here from Landing somehow. Maybe split code, cus hard to see
+// anything here already
+const SignUp = () => {
+  return <div>Sign up</div>;
 };
 
 const Friends = () => {
---
+++ src/Stripe.js
@@ -0,0 +1,71 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+
+// // Test
+// const publicKey =
+//   "pk_test_51MoTPZHbbDaXSUVRdOICX0kGZr14MGxj4DYIDvSQnRmwUv7N2G42pHHVQWU2PWvrthkwYS8RJnZZH0Vhw2K3rcST00c9sxuq8F";
+
+// Live
+const publicKey =
+  "pk_live_51MoTPZHbbDaXSUVRMJhsEjHVhGnsUGUcAXtZgF8GYpWZoVV37JXyKohVILnV2tnLnYvjlWjGKX4N5GWOzMH7TnH300aEBuf1di";
+
+// For now I just generated it with curl, needs to be on the BE tho per every transaction:
+// (-u accepts the secret key, saved in stripe_secret_key locally, but not pushed,
+// or sk_ value from the API keys on the Stripe website)
+// curl https://api.stripe.com/v1/payment_intents -d amount=100 -d currency=eur -d "payment_method_types[]"=card -u <sk_> -d "capture_method"=manual
+const clientSecret =
+  "pi_3Mt5YJHbbDaXSUVR0k9QgTHA_secret_UOctRccIAtNOPWx2LQ8pOqFki";
+
+export const stripePromise = loadStripe(publicKey);
+
+export const stripeOptions = {
+  // passing the client secret obtained from the server
+  clientSecret: clientSecret,
+};
+
+export const Stripe: React.Component = () => {
+  // Stripe
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://google.com/?q=hehe",
+      },
+    });
+
+    if (result.error) {
+      // Show error to your customer (for example, payment details incomplete)
+      console.log(result.error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      <button disabled={!stripe}> Pay </button>
+    </form>
+  );
+};
