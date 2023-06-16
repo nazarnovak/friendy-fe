befe6753ddee12c240088b2659dcf61de7078689
Nazar Novak <nazar.novac_c@sandvik.com>
2023-06-16 21:48:37 +0200

    Landing nice

--- src/Landing.js
+++ src/Landing.js
@@ -117,6 +117,30 @@ export const Landing = () => {
         <h1>Feel connected</h1>
         <h1>Feel happy</h1>
         <CTA HandleCTAClick={HandleCTAClick} />
+        <div id="down-arrow" className="top-margin-six">
+          Bottom arrow
+        </div>
+        <div className="top-margin-six">
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+        </div>
+        <div className="top-margin-six">
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+        </div>
+        <div className="top-margin-six">
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+        </div>
       </div>
     </>
   );
@@ -125,29 +149,13 @@ export const Landing = () => {
 const CTA = ({ HandleCTAClick }) => {
   return (
     <>
-      <p className="top-margin-double">
+      <p id="cta-text" className="top-margin-six">
         Tell us about who you are and what you need in a friend, and we’ll find
-        a person for you to chat with and be happy Enter your email below to
-        start your journey now
+        a person for you to chat with and be happy
       </p>
-      <button id="cta" className="top-margin-double" onClick={HandleCTAClick}>
-        Find connection
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          class="Hawkins-Icon Hawkins-Icon-Standard"
-          data-name="ChevronRight"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M7.29297 19.2928L14.5859 12L7.29297 4.70706L8.70718 3.29285L16.7072 11.2928C16.8947 11.4804 17.0001 11.7347 17.0001 12C17.0001 12.2652 16.8947 12.5195 16.7072 12.7071L8.70718 20.7071L7.29297 19.2928Z"
-            fill="currentColor"
-          ></path>
-        </svg>
+      <button id="cta" className="top-margin-triple" onClick={HandleCTAClick}>
+        <span>Find connection</span>
+        <img src="/arrow-right.svg" alt="Right arrow" />
       </button>
     </>
   );
